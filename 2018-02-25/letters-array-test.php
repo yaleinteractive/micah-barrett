9b50aae33cdaa43e9b7551e9372c81026a107d8d
Micah Barrett <mbarrett02@risd.edu>
2018-02-26 14:07:36 -0500

    nested array test
    thinking about how to further nest arrays for shapes with multiple
    strokes.

---
+++ 2018-02-25/letters-array-test.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+	
+</style>
+</head>
+<body>
+	<?php 
+	 $characters = array(
+	 	'A'=> array(
+	 			'shape'=> array(
+	 				20, 85,
+					38, 10,
+					75, 10,
+					84, 88
+	 			),
+				'shape' => array(
+					10,50,
+					90, 50
+				)
+			),
+
+	 	'B'=> array(
+	 			'shape'=> array(
+	 				10, 88,
+					8, 14,
+					60, 9,
+					77, 32,
+					9, 42,
+					85, 50,
+					88, 90,
+					28, 77,
+					13, 90
+	 			)
+		 	)
+		);
+
+
+	 
+
+
+
+	 ?>
+
+
+</body>
+</html>
