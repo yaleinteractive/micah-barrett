9a2523ea34644d54ba380b2c41ccf25d4c59ffbc
Micah Barrett <mbarrett02@risd.edu>
2018-04-14 17:31:58 -0400

    updated composition page

--- CMS-PROJECT/composition.php
+++ CMS-PROJECT/composition.php
@@ -10,22 +10,33 @@
 	$counter = 2;
 	$rotation = 30;
 	$minute = date('i');
-
+	$hour = date('G') - 4;
 	$left = $minute/2;
+	$top = $hour;
+	$scale = $hour / 10;
 
 	echo "<h1>$id</h1>";
 
+
+
 	echo "<div class='composition_wrapper'>";
-	$sql = "SELECT pieces.*, placements.* FROM pieces, placements WHERE composition_id = $id";
+	$sql = "SELECT *
+			FROM placements INNER JOIN pieces
+			WHERE placements.composition_id = $id 
+			AND pieces.id = placements.piece_id
+			ORDER BY placements.id";
 	  $result = $conn->query($sql);
 	  // If there is at least 1 row in the result, show all the rows
 	  if ($result->num_rows > 0) {
 	      // Get one row at a time until we're out of rows
 	      while ($row = $result->fetch_assoc()) {
 
-	      echo "<img class='placed-pieces' style='left:".$left."%;transform:rotate(".$rotation."deg);' src='uploads/{$row['piece_id']}/{$row['image']}'>";
+	      echo "<img class='placed-pieces' style='top:".$top."%;left:".$left."%;transform:rotate(".$rotation."deg) scale(".$scale.");' src='uploads/{$row['piece_id']}/{$row['image']}'>";
+
+
 	      $rotation++;
-	      $left = $left +1;
+
+
 
 	      }
 	  } else {
